dec07ea09265d34bf498d6339b7df376be79d81e
Andrew Vogel <andrew.vogel@elvamarketing.com>
2017-05-27 11:44:16 -0400

    Add validations for title and content.

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,5 @@
 #
 
 class Post < ApplicationRecord
+    validates :title, :content, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,12 +14,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  # Database design 
   it { is_expected.to have_db_column(:title).of_type(:string) }
   it { is_expected.to have_db_column(:content).of_type(:text) }
   it { is_expected.to have_db_column(:posted_date).of_type(:datetime) }
   it { is_expected.to have_db_column(:published)
         .of_type(:boolean).with_options(default: false) }
+  
+  # Validations
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:content) }
 
+  # Factories
   it "should have a valid public_post factory" do
     post = build(:public_post)
     expect(post).to be_valid
@@ -33,5 +39,6 @@ RSpec.describe Post, type: :model do
     expect(post.published).to eq false
     expect(post.posted_date).to be_nil
   end
+
   
 end
