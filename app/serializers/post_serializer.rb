099f6bfe5802263d774254c553af1fbfd48b9dcf
Andrew Vogel <andrewpvogel@gmail.com>
2017-07-19 14:19:10 -0400

    Parse Markdown on API call.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
+  include ApplicationHelper
   attributes :id, :content, :title, :posted_date
   belongs_to :user
   has_many :comments
+
+  def content
+    markdown(object.content)
+  end
 end
