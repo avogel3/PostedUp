2ecd8ff70668118fb23e107c71ae9eb40e6d090c
Andrew Vogel <andrewpvogel@gmail.com>
2017-07-23 23:19:45 -0400

    Generate auth token on user save.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: true
-  validates :email, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :auth_token, uniqueness: true
+  before_save :generate_auth_token
+
+  private
+  def generate_auth_token
+    if self.auth_token.blank?
+      self.auth_token = Digest::SHA1.hexdigest([Time.now, rand].join).first(30)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:auth_token) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:email) }
+  it "updates the auth token on save." do
+    user = build(:user)
+    expect(user.auth_token).to be_nil
+    user.save
+    expect(user.auth_token).not_to be_nil
+  end
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
 end
