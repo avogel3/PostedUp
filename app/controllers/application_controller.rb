43b57da12c3cbbe3e5301df13aa003942fe30c68
Andrew Vogel <andrewpvogel@gmail.com>
2017-07-23 23:36:58 -0400

    Add get_auth_token helper to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  prepend_before_filter :get_auth_token
 
-  # todo: check for auth token in the response headers as well.
   def current_user
     @current_user ||= User.find_by_auth_token(params[:auth_token]) if params[:auth_token]
   end
+  helper_method :current_user
 
   private
   # todo: test that this actually renders this error.
   def authorize
     render json: { errors: 'Please log in.' }, status: :unprocessable_entity
+  end
+
+  def get_auth_token
+    if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
+      params[:auth_token] = auth_token
+    end
   end
 end
