3377c7978d5dd760d5e8c7adf59e4097088c1c32
Andrew Vogel <andrewpvogel@gmail.com>
2017-09-28 14:19:21 -0400

    Add serializer methods for date and image.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
+  include ApplicationHelper
+
   attributes :id, :title, :content, :image, :author, :posted_at
+
+  def posted_at
+    parse_time_for_post(object.posted_at, object.user.preferred_timezone)
+  end
+
+  def image
+    object.image.url
+  end
 end
